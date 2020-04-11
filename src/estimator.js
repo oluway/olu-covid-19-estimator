f56680da9597f073f8b5d7a5c498fc2c074a0371
Ebenezer Olumide Ayeni <olu@timmalholdings.co.za>
2020-04-11 16:50:11 +0200

    commit my changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
-
-export default covid19ImpactEstimator;
+const covid19ImpactEstimator = (data) => {
+    // Challenge-1
+    const estimator = () => {
+      let days;
+      if (data.periodType === 'days') {
+        days = Math.trunc((data.timeToElapse * 1) / 3);
+      } else if (data.periodType === 'weeks') {
+        days = Math.trunc((data.timeToElapse * 7) / 3);
+      } else if (data.periodType === 'months') {
+        days = Math.trunc((data.timeToElapse * 30) / 3);
+      }
+      return days;
+    };
+    return {
+      data: {},
+      impact: {
+        currentlyInfected: data.reportedCases * 10,
+        infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** (estimator()))
+      },
+      severeImpact: {
+        currentlyInfected: data.reportedCases * 50,
+        infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** (estimator()))
+      }
+    };
+  };
+  export default covid19ImpactEstimator;
+  
