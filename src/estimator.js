9fc76e8bcbb9473f54c7b814413939d03202052e
Ebenezer Olumide Ayeni <olu@timmalholdings.co.za>
2020-04-13 17:50:23 +0200

    Changes for challenges

--- src/estimator.js
+++ src/estimator.js
@@ -28,7 +28,7 @@ const covid19ImpactEstimator = (data) => {
       hospitalBedsByRequestedTime: Math.trunc(expectedBed - impact15),
       casesForICUByRequestedTime: impact5,
       casesForVentilatorsByRequestedTime: impact2,
-      dollarsInFlight: (impactInfection * 0.65) * 1.5 * (2 ** (estimator()))
+      dollarsInFlight: (impactInfection * 0.65) * 1.5
     },
     severeImpact: {
       currentlyInfected: data.reportedCases * 50,
@@ -37,7 +37,7 @@ const covid19ImpactEstimator = (data) => {
       hospitalBedsByRequestedTime: Math.trunc(expectedBed - severe15),
       casesForICUByRequestedTime: severe5,
       casesForVentilatorsByRequestedTime: severe2,
-      dollarsInFlight: (severeInfection * 0.65) * 1.5 * (2 ** (estimator()))
+      dollarsInFlight: (severeInfection * 0.65) * 1.5
     }
   };
 };
